7ec0e2413603e697e94669a3a86daf940faf2e86
MKashanButt <kashanbutt566@gmail.com>
2025-04-25 22:02:59 +0500

    updated theme, added toastr notifications, and stoped context menu from opening

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,19 +8,35 @@
         <h1 class="text-4xl font-bold text-center mb-5">Medicare
             Dupe
             Checker</h1>
-        <form action="/check-medicare-id" class="flex gap-2">
+        <form action="/check-medicare-id" class="flex gap-2 border-y-2 py-10">
             <input type="text" name="medicare_id" id="medicare_id"
-                class="w-[80%] border-2 border-black pl-2 outline-none w-4/5" placeholder="Enter Medicare Id">
-            <button class="p-2 flex items-center justify-center bg-black text-white uppercase w-1/5">Check
+                class="w-[80%] border-2 border-white pl-2 outline-none w-4/5 text-black" placeholder="Enter Medicare Id"
+                maxlength="11" pattern=".{11,11}" title="Medicare ID must be exactly 11 characters">
+            <button class="p-2 flex items-center justify-center bg-white text-black uppercase w-1/5">Check
                 Id</button>
         </form>
-        @isset($present)
-            @if ($present)
-                <p class="bg-red-500 w-full p-2 text-center text-white uppercase font-bold">Member Id is Present</p>
-            @else
-                <p class="bg-green-500 w-full p-2 text-center text-white uppercase font-bold">Member Id is not Present
-                </p>
-            @endif
-        @endisset
+        <p class="text-center">Copyright © Squad International Pvt Ltd. All Rights Reserved</p>
+        <script>
+            @isset($present)
+                @if ($present)
+                    toastr.danger('Member Id is Present!');
+                @else
+                    toastr.success('Member Id is not Present!');
+                @endif
+            @endisset
+
+            document.addEventListener('contextmenu', function(e) {
+                e.preventDefault();
+            });
+
+            document.addEventListener('keydown', function(e) {
+                if (e.key === 'F12' ||
+                    (e.ctrlKey && e.shiftKey && e.key === 'I') ||
+                    (e.ctrlKey && e.shiftKey && e.key === 'J') ||
+                    (e.ctrlKey && e.key === 'U')) {
+                    e.preventDefault();
+                }
+            });
+        </script>
     </section>
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,10 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
+
+    {{-- Toastr --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 </head>
 
 <body>
-    <main class="flex items-center justify-center h-[100vh] bg-[linear-gradient] text-white">
+    <main class="flex items-center justify-center h-[100vh] bg-black text-white select-none">
         @yield('content')
     </main>
 </body>
